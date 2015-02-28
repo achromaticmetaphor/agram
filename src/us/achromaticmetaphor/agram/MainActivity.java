e24bd7e79c674b542cbbaa45eacb226c782d79b8
Charles Cagle <achromaticmetaphor@gmail.com>
2015-02-27 20:23:02 -0500

    remember previously loaded wordlists

--- src/us/achromaticmetaphor/agram/MainActivity.java
+++ src/us/achromaticmetaphor/agram/MainActivity.java
@@ -2,6 +2,7 @@ package us.achromaticmetaphor.agram;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
@@ -11,17 +12,30 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends Activity {
 
   private static String menuAbout = "About";
   private static final int REQUEST_FILEBROWSER = 1;
+  private static final String builtinWordlist = "SCOWL (built-in)";
   private static final String selectedWordlistKey = "wordlist.label";
+  private static final String wordlistsFilename = "wordlists";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -58,8 +72,38 @@ public class MainActivity extends Activity {
     }
   }
 
+  private Set<String> getWordlists() {
+    Set<String> wordlists = new HashSet<String>();
+    wordlists.add(builtinWordlist);
+    try {
+      DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream(new File(getFilesDir(), wordlistsFilename))));
+      int len = dis.readInt();
+      for (int i = 0; i < len; i++)
+        wordlists.add(dis.readUTF());
+      dis.close();
+    }
+    catch (IOException e) {}
+    return wordlists;
+  }
+
+  private void setWordlists(Collection<String> wordlists) {
+    try {
+      DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(new File(getFilesDir(), wordlistsFilename))));
+      dos.writeInt(wordlists.size());
+      for (String s : wordlists)
+        dos.writeUTF(s);
+      dos.close();
+    }
+    catch (IOException e) {}
+  }
+
   private void enrollWordlist(String label) {
     getSharedPreferences("us.achromaticmetaphor.agram", MODE_PRIVATE).edit().putString(selectedWordlistKey, label).commit();
+    Set<String> wordlists = getWordlists();
+    if (! wordlists.contains(label)) {
+      wordlists.add(label);
+      setWordlists(wordlists);
+    }
   }
 
   private void wordlistFail(String label) {
@@ -67,6 +111,35 @@ public class MainActivity extends Activity {
     build.setTitle("Error");
     build.setMessage("Failed to load wordlist: " + label);
     build.setPositiveButton("OK", null);
+    build.show();
+  }
+
+  private void chooseWordlist() {
+    Set<String> wordlists = getWordlists();
+    final CharSequence [] wordlistsArray = wordlists.toArray(new CharSequence [0]);
+    Arrays.sort(wordlistsArray);
+    final int selected [] = {Arrays.binarySearch(wordlistsArray, getSharedPreferences("us.achromaticmetaphor.agram", MODE_PRIVATE).getString(selectedWordlistKey, builtinWordlist))};
+    if (selected[0] < 0)
+      selected[0] = -1;
+
+    AlertDialog.Builder build = new AlertDialog.Builder(this);
+    build.setTitle("Choose wordlist");
+    build.setSingleChoiceItems(wordlistsArray, selected[0], new DialogInterface.OnClickListener() {
+      public void onClick(DialogInterface di, int i) {
+        selected[0] = i;
+      }
+    });
+    build.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+      public void onClick(DialogInterface di, int i) {
+        loadWordlist("", (String) wordlistsArray[selected[0]]);
+      }
+    });
+    build.setNeutralButton("Choose file", new DialogInterface.OnClickListener() {
+      public void onClick(DialogInterface di, int i) {
+        startActivityForResult(new Intent(MainActivity.this, FileBrowser.class), REQUEST_FILEBROWSER);
+      }
+    });
+    build.setNegativeButton("Cancel", null);
     build.show();
   }
 
@@ -97,15 +170,26 @@ public class MainActivity extends Activity {
     if (mi.getTitle().equals(menuAbout))
       startActivity(new Intent(this, About.class));
     if (mi.getTitle().equals("Select wordlist"))
-      startActivityForResult(new Intent(this, FileBrowser.class), REQUEST_FILEBROWSER);
+      chooseWordlist();
     return true;
   }
 
   @Override
   protected void onActivityResult(int rcode, int result, Intent data) {
     if (result == RESULT_OK && rcode == REQUEST_FILEBROWSER) {
-      String filename = data.getStringExtra(FileBrowser.filenameKey);
-      loadWordlist(filename, filename);
+      final String filename = data.getStringExtra("us.achromaticmetaphor.agram.FileBrowser.filename");
+      AlertDialog.Builder build = new AlertDialog.Builder(this);
+      final EditText edit = new EditText(this);
+      edit.setText(new File(filename).getName());
+      build.setTitle("Enter label for wordlist.");
+      build.setView(edit);
+      build.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface di, int i) {
+          loadWordlist(filename, edit.getText().toString());
+        }
+      });
+      build.setNegativeButton("Cancel", null);
+      build.show();
     }
   }
 
