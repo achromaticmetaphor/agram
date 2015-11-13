7c9757879337486c946438ed24db10d2cfc8261f
Charles Cagle <achromaticmetaphor@gmail.com>
2015-11-13 05:37:47 -0500

    add CFLAGS to control dialect and diagnostics

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := anagram
 LOCAL_SRC_FILES := agram_wc.c anagram.c anagrams.c astr.c is_anagram.c is_within.c jni.c lcwc.c lettercounts.c vector.c wc.c wl_upgrade.c words_from.c
-LOCAL_CFLAGS := -DPLATFORM='"$(TARGET_ARCH_ABI)"' -DAGRAM_ANDROID=1
+LOCAL_CFLAGS := -DPLATFORM='"$(TARGET_ARCH_ABI)"' -DAGRAM_ANDROID=1 -std=iso9899:1999 -Wall -Wextra -Wno-unused-parameter
 
 include $(BUILD_SHARED_LIBRARY)
