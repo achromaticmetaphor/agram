a2010bfc376935a9cbf34867b28e442b5478f733
Charles Cagle <achromaticmetaphor@gmail.com>
2016-03-31 14:32:57 -0400

    set locale for command interface

--- jni/agram.c
+++ jni/agram.c
@@ -1,5 +1,6 @@
 #define _POSIX_C_SOURCE 200809L
 
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -67,6 +68,8 @@ static int contained(const struct wordlist * const wl, int argc, char * argv[])
 
 int main(int argc, char * argv[])
 {
+  setlocale(LC_ALL, "");
+
   struct wordlist wl;
   if (init_wl(&wl))
     return 1;
