98dd0d2cf71c89a3adf9d4f3d41180953395902e
Charles Cagle <achromaticmetaphor@gmail.com>
2017-05-23 01:02:30 -0400

    refactor lettercounts_s

--- csrc/lettercounts.cc
+++ csrc/lettercounts.cc
@@ -1,8 +1,8 @@
 #define _POSIX_C_SOURCE 200809L
 
-#include <algorithm>
 #include <cstddef>
 #include <cstdlib>
+#include <map>
 #include <vector>
 
 #include "agram_types.h"
@@ -11,28 +11,16 @@
 #include "astr.h"
 #endif
 
-static void insert(const agram_cpt c, std::vector<agram_cpt> & l)
+static void lettercounts(std::vector<unsigned int> & counts, std::vector<agram_cpt> & letters, const agram_cpt * const str)
 {
-  for (auto e : l)
-    if (e == c)
-      return;
-  l.push_back(c);
-}
+  std::map<agram_cpt, size_t> cmap;
+  for (auto s = str; *s; s++)
+    cmap[*s]++;
 
-static void lettercounts_s(std::vector<unsigned int> & counts, std::vector<agram_cpt> & letters, const agram_cpt * const str, const size_t len)
-{
-  const agram_cpt * s;
-
-  for (s = str; *s; s++)
-    insert(*s, letters);
-  std::sort(letters.begin(), letters.end());
-
-  for (size_t i = 0; i < letters.size(); ++i)
+  for (auto & p : cmap)
     {
-      counts.push_back(0);
-      for (size_t j = 0; j < len; ++j)
-        if (letters[i] == str[j])
-          counts[i]++;
+      letters.push_back(p.first);
+      counts.push_back(p.second);
     }
 }
 
@@ -40,11 +28,12 @@ void lettercounts(std::vector<unsigned int> & counts, std::vector<agram_cpt> & l
 {
 #if AGRAM_ANDROID
   agram_cpt tmp[slen + 1];
-  lettercounts_s(counts, letters, tmp, uastrcpy(tmp, str, slen));
+  uastrcpy(tmp, str, slen);
+  lettercounts(counts, letters, tmp);
 #else
   const size_t wlen = mbstowcs(nullptr, str, 0);
   agram_cpt tmp[wlen + 1];
   mbstowcs(tmp, str, wlen + 1);
-  lettercounts_s(counts, letters, tmp, wlen);
+  lettercounts(counts, letters, tmp);
 #endif
 }
